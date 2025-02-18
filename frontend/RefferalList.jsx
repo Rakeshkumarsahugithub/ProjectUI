f924c75e1ebb49fb214dfc9bde71a0ae9bceae53
Rakesh Kumar Sahu <131417933+Rakeshkumarsahugithub@users.noreply.github.com>
2025-02-19 00:40:34 +0530

    Update RefferalList.jsx

--- frontend/RefferalList.jsx
+++ frontend/RefferalList.jsx
@@ -106,7 +106,7 @@ const ReferralList = () => {
             </div>
 
             <div className="dropdown">
-              <img src="https://th.bing.com/th/id/OIP.3F-fdDamE0-O8TsyCIP4vQHaHa?w=174&h=180&c=7&r=0&o=5&dpr=2&pid=1.7" alt="Shortcuts" />
+              <img src="frontend/Screenshot 2024-09-07 041455.png" alt="Shortcuts" />
               <div className="dropdown-content">
                 <a href="#">Calender</a>
                 <a href="#">User App</a>
